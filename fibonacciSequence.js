2fc5cd33b4800a6f09305ace740803190fa6322c
Gihozo23 <christellegihozo23@gmail.com>
2024-08-25 16:30:40 +0200

    chore: refactor the fibonacciSequence function

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -1,6 +1,6 @@
-const fibonacciSequence = (n) => {
-  if (n < 0 || n === undefined) return "Please enter a positive integer";
-  if (n === 0) return [0];
+const fibonacciSequence = (numebr) => {
+  if (number < 0 || number === undefined) return "Please enter a positive integer";
+  if (number === 0) return [0];
   const fibonacciArray = [0, 1];
   for (let i = 2; i <= n; i++) {
     fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
